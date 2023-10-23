dc689749ffdbac48f3ad6f05dc836d623c94344b
maxoleksa <maxdoleksa@gmail.com>
2023-10-23 16:42:46 -0500

    restarting ... model_comp begin // model del

--- model_class.cpp
+++ model_class.cpp
@@ -6,59 +6,9 @@ using namespace std;
 
 class Model {
     private:
-        vector<vector<double>> outputs;
-        vector<vector<double>> inputs;
-        double y;
     public:
-        double h;
-        double learning_rate;
-        vector<Layer> layers;
-        vector<Weight> weights;
-        vector<vector<double>> deltas;
-        vector<string> activations;
-        vector<vector<double>> biases;
-
-        void compute_deltas(int layer) {
-            double tmp;
-            if (layer == size(layers)) {
-                return outputs.back() - y;
-            } else {
-                for (int row = 0; row < size(weights[layer+1][row]); row++) {
-                    for (int col = 0; col < size(weights[layer+1]); col++) {
-                        tmp = weights[layer+1][col][row] * deltas[layer+1][row][col]
-                    }
-                }
-            }
-        }
-
-        double forward_propagation(vector<double> input_data) {
-            inputs.push_back(input_data);
-
-            for (int layer = 0; layer < size(layers); layer++) {
-                vector<double> tmp_vec;
-                double tmp_val = 0.0;
-
-                outputs.push_back(activation(inputs[layer]));
-
-                for (int row = 0; row < size(weights[layer]); row++) {
-                    for (int col = 0; col < size(weights[layer][row]); col++) {
-                        tmp_val += weights[layer][row][col]*outputs[layer][col] + biases[layer][col];
-                    }
-                    tmp_vec.push_back(tmp_val);
-                }
-
-                inputs.push_back(tmp_vec);
-
-                delete &tmp_vec;
-                delete &tmp_val;
-            }
-
-            return outputs.back()
-        }
-
-        
 };
 
 int main() {
-    return 0
+    return 0;
 }
--- model_component_classes.cpp
+++ model_component_classes.cpp
@@ -5,81 +5,72 @@
 using namespace std;
 
 
+class Activation { 
+    /*
+    (i) function definitions for multiple activation functions and their derivatives
+    (ii) setter functions, used in the constructor, that set the desires activation function as the actual activation function
+    (iii) getter functions that reference the activation function and its derivative and compute desired values
+        ^^ these will be referenced in the layer class
+    */
+};
+
 class Weight {
     private:
-        int next_nodes;
-        int prev_nodes;
-        vector<double> inputs;
-        vector<double> outputs;
     public:
-        double weights[next_nodes][prev_nodes];
+        vector<double> weights; // dim = [next.nodes,prev.nodes] ... use prev.nodes as spacer for referencing values (e.g. idx = [row]*prev.nodex + [col])
+        Layer prev_layer;
+        Layer next_layer;
+        vector<double> bias; // 1d vector of size next_layer.nodes
 
-        Weight(Layer prev_layer, Layer next_layer) {
-            next_nodes = next_layer.num_nodes;
-            prev_nodes = prev_layer.num_nodes;
+        // constructor
+        Weight(Layer _prev, Layer _next) {
+            prev_layer = _prev;
+            next_layer = _next;
 
-            for (int i = 0; i < next_nodes; i++) {
-                for (int j = 0; j < prev_nodes; j++) {
-                    weights[i][j] = .1;
-                }
+            for (int _ = 0; _ < prev_layer.nodes*next_layer.nodes; _++) {
+                weights.push_back(.1); // initial weight value, can randomize or make distribution rather than single value
+                if (_ < next_layer.nodes) {bias.push_back(.1);} // initial bias value 
             }
         }
 
-        vector<double> generateOutputs(vector<double> inputs = inputs) {
+        // forward propagation
+        vector<double> computeInput(vector<double> output) { // output is generated in the Layer class through the activation function ... output = activation(input)
+            vector<double> output;
             
+            for (int row = 0; row < next_layer.nodes; row++) {
+                double tmp = 0;
+
+                for (int col = 0; col < prev_layer.nodes; col++) {
+                    tmp += weights[row*prev_layer.nodes + col] * output[col] + bias[col];
+                }
+
+                output.push_back(tmp);
+                tmp = 0;
+            }
         }
+
+        // back propagation will be defined, computed, and re-assigning all in model class (since it requires information from other layers/weights)
 };
 
-class Layer {
+class Layer { // parent class for (i) hidden layers ... (a) fully connected (b) not-fully (c) etc ... (ii) input layers (iii) output layers
     private:
-        double input[num_nodes];
-        double output[num_nodes];
     public:
-        int num_nodes;
-        double bias[num_nodes];
-        string activation;
-        double learning_rate;
+        int nodes;
 
-        // constructor 
-        Layer(int _n, string _activation = "sigmoid", double _lr = .1) {
-            num_nodes = _n;
-            activation = _activation;
-            learning_rate = _lr;
-        }
-
-        double forwardPropagation() {
-            cout << "Not implemented.";
-            throw; 
-        }
-        double backwardPropagation() {
-            cout << "Not implemented.";
-            throw;
-        }
 };
 
-class Dense : public Layer {
-    public:
-        Dense(int _n, string _activation = "sigmoid", double _lr = .1) {
-            num_nodes = _n;
-            activation = _activation;
-            learning_rate = _lr;
-        }
+class HiddenLayer : Layer { // can have children (e.g. fully-connected/dense, sparsely-connected)
 
-        double forwardPropagation(double input[num_nodes]) { 
-            for (int i = 0; i < num_nodes; i++) {
-                output[i] = activation(input[i]);
-            }
-            return output;
-        }
-
-        double backwardPropagation(double delta[],double lr) {
-            for (int i = 0; i < num_nodes; i++) {
-                for (int j = 0; j <)
-            }
-        }
 };
 
+class InputLayer : Layer {
+
+};
+
+class OutputLayer : Layer {
+
+};
 
 int main() {
-    return 0
+    return 0;
 }
